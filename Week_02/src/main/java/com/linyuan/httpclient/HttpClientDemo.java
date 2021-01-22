8c97531f661cc895ba6025a738919da6235b0a7a
xideng <462726044@qq.com>
2021-01-22 17:43:40 +0800

    HttpClientDemo

--- Week_02/src/main/java/com/linyuan/httpclient/HttpClientDemo.java
+++ Week_02/src/main/java/com/linyuan/httpclient/HttpClientDemo.java
@@ -1,11 +1,15 @@
 package com.linyuan.httpclient;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPatch;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 
@@ -19,11 +23,19 @@ public class HttpClientDemo {
 
     public static void main(String[] args) throws IOException {
         // 创建HttpClient
-        HttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpClient httpClient = HttpClients.createDefault();
         // 发送请求建立连接 get请求
-        HttpGet httpGet = new HttpGet(URL);
+        HttpGet httpGet = new HttpGet("http://localhost:8801");
         // 获得本地8801的结果
-        HttpResponse response = httpClient.execute(httpGet);
-        System.out.println(response);
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+        if (response.getStatusLine().getStatusCode() == 200) {
+            HttpEntity entity = response.getEntity();
+
+            String entityString = EntityUtils.toString(entity, "utf-8");
+            System.out.println(entityString);
+        }
+        response.close();
+        httpClient.close();
+        //System.out.println(response);
     }
 }
